4d040aaf4658ce369197e1f7d671e889ff15290b
Jhana Gustave <jhana.gustave@gmail.com>
2019-04-24 19:50:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,13 +18,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  i = 0
-  if  array.any? {|long| long.length > 4}
-    return true
-  else
-    return false
-  i += 1
-  end
+  
 end
 
 
